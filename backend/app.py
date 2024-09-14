484a7a38ecc5a9a959dc484ea85301e3e8c28f66
YashK2005 <yashkoth7@gmail.com>
2024-09-14 15:15:07 -0400

    starting chroma

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 import vertexai
+import chromadb
 
 from vertexai.vision_models import MultiModalEmbeddingModel, Video
 from vertexai.vision_models import VideoSegmentConfig
 
 # TODO(developer): Update project_id and location
-vertexai.init(project='spaceshtn', location="us-central1")
+vertexai.init(project='spaceshtn', location="northamerica-northeast1")
 
 model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding")
 
@@ -25,3 +26,32 @@ for video_embedding in embeddings.video_embeddings:
     print(f"Embedding: {video_embedding.embedding}")
 
 print(f"Text Embedding: {embeddings.text_embedding}")
+
+chroma_client = chromadb.Client()
+# switch `create_collection` to `get_or_create_collection` to avoid creating a new collection every time
+collection = chroma_client.get_or_create_collection(name="my_collection")
+# switch `add` to `upsert` to avoid adding the same documents every time
+collection.upsert(
+    documents=[
+        "This is a document about pineapple",
+        "This is a document about oranges"
+    ],
+    ids=["id1", "id2"]
+)
+
+collection.upsert(
+    documets=["doc 3"],
+    embeddings=[embeddings.video_embeddings[0].embedding],
+    ids=["id3"]
+)
+
+results = collection.query(
+    query_texts=["This is a query document about florida"], # Chroma will embed this for you
+    n_results=2 # how many results to return
+)
+
+results = collection.query(
+    query_texts=["This is a query document about cars driving"], # Chroma will embed this for you
+    n_results=2 # how many results to return
+)
+print(results)
