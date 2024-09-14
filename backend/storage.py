7a9f67220ad38ac4d60621ca0e84e69c2e7a77e2
Ilia Mireskandari <mrskndri@gmail.com>
2024-09-14 15:36:02 -0400

    change

---
+++ backend/storage.py
@@ -0,0 +1,12 @@
+'''
+
+TODO:
+add_uri_to_local_database(uri) -> None
+get_video_embedding_from_google(uri) -> embedding
+add_embedding_to_chroma(embedding) -> None
+get_text_embedding_from_google(text) -> embedding
+find_closest_embedding_from_embedding(embedding) -> [uris, metadata]
+
+
+'''
+
