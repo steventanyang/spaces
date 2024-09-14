a9b0ecde7f26586b67aa415e0e793c8d6f4b213b
Ilia Mireskandari <mrskndri@gmail.com>
2024-09-14 15:26:46 -0400

    resolved package conflicts

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,31 @@
 import vertexai
 import chromadb
 
-from vertexai.vision_models import MultiModalEmbeddingModel, Video
-from vertexai.vision_models import VideoSegmentConfig
+# from vertexai.vision_models import MultiModalEmbeddingModel, Video
+# from vertexai.vision_models import VideoSegmentConfig
 
-# TODO(developer): Update project_id and location
-vertexai.init(project='spaceshtn', location="northamerica-northeast1")
+# # TODO(developer): Update project_id and location
+# vertexai.init(project='spaceshtn', location="northamerica-northeast1")
 
-model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding")
+# model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding")
 
-embeddings = model.get_embeddings(
-    video=Video.load_from_file(
-        "gs://cloud-samples-data/vertex-ai-vision/highway_vehicles.mp4"
-    ),
-    video_segment_config=VideoSegmentConfig(end_offset_sec=1),
-    contextual_text="Cars on Highway",
-)
+# embeddings = model.get_embeddings(
+#     video=Video.load_from_file(
+#         "gs://cloud-samples-data/vertex-ai-vision/highway_vehicles.mp4"
+#     ),
+#     video_segment_config=VideoSegmentConfig(end_offset_sec=1),
+#     contextual_text="Cars on Highway",
+# )
 
-# Video Embeddings are segmented based on the video_segment_config.
-print("Video Embeddings:")
-for video_embedding in embeddings.video_embeddings:
-    print(
-        f"Video Segment: {video_embedding.start_offset_sec} - {video_embedding.end_offset_sec}"
-    )
-    print(f"Embedding: {video_embedding.embedding}")
+# # Video Embeddings are segmented based on the video_segment_config.
+# print("Video Embeddings:")
+# for video_embedding in embeddings.video_embeddings:
+#     print(
+#         f"Video Segment: {video_embedding.start_offset_sec} - {video_embedding.end_offset_sec}"
+#     )
+#     print(f"Embedding: {video_embedding.embedding}")
 
-print(f"Text Embedding: {embeddings.text_embedding}")
+# print(f"Text Embedding: {embeddings.text_embedding}")
 
 chroma_client = chromadb.Client()
 # switch `create_collection` to `get_or_create_collection` to avoid creating a new collection every time
@@ -39,11 +39,11 @@ collection.upsert(
     ids=["id1", "id2"]
 )
 
-collection.upsert(
-    documets=["doc 3"],
-    embeddings=[embeddings.video_embeddings[0].embedding],
-    ids=["id3"]
-)
+# collection.upsert(
+#     documents=["doc 3"],
+#     embeddings=[embeddings.video_embeddings[0].embedding],
+#     ids=["id3"]
+# )
 
 results = collection.query(
     query_texts=["This is a query document about florida"], # Chroma will embed this for you
