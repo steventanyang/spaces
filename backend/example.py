d94c011528ed9994059bcb2afeecb2a7df836565
Ilia Mireskandari <mrskndri@gmail.com>
2024-09-14 20:22:52 -0400

    correct embeddings

--- backend/example.py
+++ backend/example.py
@@ -7,51 +7,32 @@ from vertexai.vision_models import VideoSegmentConfig
 # TODO(developer): Update project_id and location
 vertexai.init(project='spaceshtn', location="northamerica-northeast1")
 
-# model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding")
+model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding")
 
-# embeddings = model.get_embeddings(
-#     video=Video.load_from_file(
-#         "gs://cloud-samples-data/vertex-ai-vision/highway_vehicles.mp4"
-#     ),
-#     video_segment_config=VideoSegmentConfig(end_offset_sec=1),
-#     contextual_text="Cars on Highway",
-# )
+embeddings = model.get_embeddings(
+    video=Video.load_from_file(
+        "gs://cloud-samples-data/vertex-ai-vision/highway_vehicles.mp4"
+    ),
+)
 
-# # Video Embeddings are segmented based on the video_segment_config.
-# print("Video Embeddings:")
-# for video_embedding in embeddings.video_embeddings:
-#     print(
-#         f"Video Segment: {video_embedding.start_offset_sec} - {video_embedding.end_offset_sec}"
-#     )
-#     print(f"Embedding: {video_embedding.embedding}")
+# Video Embeddings are segmented based on the video_segment_config.
+print("Video Embeddings:")
+for video_embedding in embeddings.video_embeddings:
+    print(
+        f"Video Segment: {video_embedding.start_offset_sec} - {video_embedding.end_offset_sec}"
+    )
+    print(f"Embedding: {video_embedding.embedding}")
 
-# print(f"Text Embedding: {embeddings.text_embedding}")
+print(f"Text Embedding: {embeddings.text_embedding}")
 
-chroma_client = chromadb.Client()
+chroma_client = chromadb.PersistentClient("test")
 # switch `create_collection` to `get_or_create_collection` to avoid creating a new collection every time
 collection = chroma_client.get_or_create_collection(name="my_collection")
 # switch `add` to `upsert` to avoid adding the same documents every time
 collection.upsert(
-    documents=[
-        "This is a document about pineapple",
-        "This is a document about oranges"
-    ],
-    ids=["id1", "id2"]
+    documents=["doc 3"],
+    embeddings=[embeddings.video_embeddings[0].embedding],
+    ids=["id3"]
 )
 
-# collection.upsert(
-#     documents=["doc 3"],
-#     embeddings=[embeddings.video_embeddings[0].embedding],
-#     ids=["id3"]
-# )
-
-results = collection.query(
-    query_texts=["This is a query document about florida"], # Chroma will embed this for you
-    n_results=2 # how many results to return
-)
-
-results = collection.query(
-    query_texts=["This is a query document about cars driving"], # Chroma will embed this for you
-    n_results=2 # how many results to return
-)
 print(results)
